016ccd317dccf2cb54e597e1421dd9127331d842
chrischeung1126 <87195612+chrischeung1126@users.noreply.github.com>
2021-11-21 17:03:53 +0800

    Update install_fxlogics.ps1

--- install_fxlogics.ps1
+++ install_fxlogics.ps1
@@ -4,8 +4,27 @@ $FSlogixZipFolder = $RootFolder+"FSLogix_Apps_2.9.7979.62170.zip"
 $FSlogixInstallFolder = $RootFolder+"Install"
 $FSlogixInstallerPath = $FSlogixInstallFolder+"\x64\Release\FSLogixAppsSetup.exe"
 
-#Create Folder structure
-if (!(Test-Path -Path $RootFolder)){New-Item -Path $RootFolder -ItemType Directory}
+
+try {
+    #Create Folder structure
+    if (!(Test-Path -Path $RootFolder)){New-Item -Path $RootFolder -ItemType Directory}
+}
+catch {
+    $errMsg = $_.Exception.message
+    Write-Error ("Cannot Create the FSlogicx Folder: " + $errMsg)
+    Break
+}
+
+
+try {
+    #Download all source file async and wait for completion
+    Invoke-WebRequest -Uri "https://aka.ms/fslogix/download" -OutFile $FSlogixZipFolder
+}
+catch {
+    $errMsg = $_.Exception.message
+    Write-Error ("Cannot Download the FSlogix: " + $errMsg)
+    Break
+}
 
 #Configure logging
 function log
@@ -14,18 +33,68 @@ function log
    "`n`n$(get-date -f o)  $message" 
 }
 
-#Download all source file async and wait for completion
-Invoke-WebRequest -Uri "https://aka.ms/fslogix/download" -OutFile $FSlogixZipFolder
+#Unzip the FSlogicx Folder
+Log "Unzip the FSlogicx Folder"
+Expand-Archive -Path $FSlogixZipFolder -DestinationPath $FSlogixInstallFolder
 
+try {
+    #Install the FSlogicx
+    Log "Install the FSlogicx Folder"
+    Start-Process -Wait -FilePath $FSlogixInstallerPath -ArgumentList "/install /quiet /norestart" -PassThru
+}
+catch {
+    $errMsg = $_.Exception.message
+    Write-Error ("Cannot Install the FSlogix: " + $errMsg)
+    Break
+}
+
+#Set Variables
+$RootFolder = "D:\FSLogix\"
+$FSlogixZipFolder = $RootFolder+"FSLogix_Apps_2.9.7979.62170.zip"
+$FSlogixInstallFolder = $RootFolder+"Install"
+$FSlogixInstallerPath = $FSlogixInstallFolder+"\x64\Release\FSLogixAppsSetup.exe"
+
+
+try {
+    #Create Folder structure
+    if (!(Test-Path -Path $RootFolder)){New-Item -Path $RootFolder -ItemType Directory}
+}
+catch {
+    $errMsg = $_.Exception.message
+    Write-Error ("Cannot Create the FSlogicx Folder: " + $errMsg)
+    Break
+}
+
+
+try {
+    #Download all source file async and wait for completion
+    Invoke-WebRequest -Uri "https://aka.ms/fslogix/download" -OutFile $FSlogixZipFolder
+}
+catch {
+    $errMsg = $_.Exception.message
+    Write-Error ("Cannot Download the FSlogix: " + $errMsg)
+    Break
+}
+
+#Configure logging
+function log
+{
+   param([string]$message)
+   "`n`n$(get-date -f o)  $message" 
+}
 
 #Unzip the FSlogicx Folder
 Log "Unzip the FSlogicx Folder"
 Expand-Archive -Path $FSlogixZipFolder -DestinationPath $FSlogixInstallFolder
 
-#Install the FSlogicx
-Log "Install the FSlogicx Folder"
-Start-Process -Wait -FilePath $FSlogixInstallerPath -ArgumentList "/install /quiet /norestart" -PassThru
+try {
+    #Install the FSlogicx
+    Log "Install the FSlogicx Folder"
+    Start-Process -Wait -FilePath $FSlogixInstallerPath -ArgumentList "/install /quiet /norestart" -PassThru
+}
+catch {
+    $errMsg = $_.Exception.message
+    Write-Error ("Cannot Install the FSlogix: " + $errMsg)
+    Break
+}
 
-#Remove Root Folder
-#Log "Remove the FSlogicx Folder"
-#Remove-Item -Path $RootFolder -Force
